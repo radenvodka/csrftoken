b7beb7a4f1907e483b23e014b8b7ef6aab826f8c
RadenVodka (Eka S) <ekasyahwan.dev@gmail.com>
2018-11-14 16:58:15 +0700

    Update class.csrftoken.php

--- example/class.csrftoken.php
+++ example/class.csrftoken.php
@@ -4,12 +4,12 @@ error_reporting(0);
  * @Author: Eka Syahwan
  * @Date:   2017-05-27 06:52:42
  * @Last Modified by:   Eka Syahwan
- * @Last Modified time: 2017-05-27 06:58:47
+ * @Last Modified time: 2018-11-14 16:54:26
  */
 class CSRFToken
 {
 	public function generate_token(){
-		$_SESSION['token']  = "__csrfToken:".md5(date("dmY h:i:s").rand(10000,90000));
+		$_SESSION['token']  = "csrf_token:".md5(password_hash( date("dmY h:i:s").rand(10000,90000),PASSWORD_DEFAULT));
 		return $_SESSION['token'];
 	}
 	public function show_tokenHTML(){
@@ -17,8 +17,14 @@ class CSRFToken
 	}
 	public function validateToken($token){
 		if($token != $_SESSION['token']){
+			
+			$this->generate_token();
+
 			return false;
 		}else{
+
+			$this->generate_token();
+			
 			return true;
 		}
 	}
